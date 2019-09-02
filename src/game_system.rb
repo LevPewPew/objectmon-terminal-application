9ff5dc720402c7f4ed6235de7c707a14a38d26e2
LevPewPew <levtoth1988@gmail.com>
2019-09-02 22:40:13 +1000

    added case for player trying to move off grid. added the spawning of a objectmon on a map tile. began objectmon class. some bug fixes. begin manu system class

--- src/game_system.rb
+++ src/game_system.rb
@@ -1,48 +1,160 @@
+require 'colorize'
+require 'artii'
+
 def run_game
-    map_grid = initialize_map
-    map_grid[0][0].player_is_here = true
-    display_map(map_grid)
+    puts ''
+    artii = Artii::Base.new :font => 'slant'
+    puts artii.asciify('Objectmon!')
+    puts ''
+    # FIXME hard coded, fix if time
+    puts 'Make your way to the South-East tile to win!'
+    puts ''
+    map = Map.new
+    map.map_grid[0][0].player_is_here = true
+    map.display_map
+
+    Menu.menu_system(map)
+end
+
+class Menu
+    def self.menu_system(map)
+        loop do
+            # choose and then  move in a direction on the map, game is won if player reaches the winning tile
+            choice = menu_ask_direction
+            current_location = map.move_location(choice)
+            map.display_map
+            if map.map_grid[map.winning_tile[0]][map.winning_tile[1]].player_is_here
+                puts 'Congratulations! You made it! You won!'.colorize(:color => :black, :background => :light_green)
+                exit
+            end
+
+            # check if a wild objectmon appears and begin fight if so
+            if map.map_grid[current_location[0]][current_location[1]].wild_objectmon
+                puts 'PLACEHOLDER begin fight!'
+                puts ''
+            end
+
+
+
+
+        end
+        # PLACEHOLDER
+        return
+    end
+
+    def self.menu_ask_direction
+        puts '***********************************************************'
+        puts '         What direction would you like to move in?         '.colorize(:color => :black, :background => :white)
+        puts '-----------------------------------------------------------'
+        puts '1. North'.colorize(:cyan)
+        puts '2. South'.colorize(:cyan)
+        puts '3. East'.colorize(:cyan)
+        puts '4. West'.colorize(:cyan)
+        puts '***********************************************************'
+        print '> '
+        choice = gets.strip.to_i
+        puts ''
+        case choice
+        when 1
+            return 'north'
+        when 2
+            return 'south'
+        when 3
+            return 'east'
+        when 4
+            return 'west'
+        else
+            p "invalid choice"
+        end
+    end
+
+    private_class_method(:menu_ask_direction)
 end
 
 # map array grid is top to bottom left to right
-# map_tile_x_y where x is the column and y is the row
-# so map_tile_2_1 would represent the third column, second row
-def initialize_map
-    # FIXME hard coded instances for now, if time will make these more random
-    map_tile_0_0 = MapTile.new('mountain', false)
-    map_tile_1_0 = MapTile.new('grass', false)
-    map_tile_2_0 = MapTile.new('mountain', false)
-    map_tile_0_1 = MapTile.new('grass', false)
-    map_tile_1_1 = MapTile.new('grass', false)
-    map_tile_2_1 = MapTile.new('mountain', false)
-    map_tile_0_2 = MapTile.new('mountain', false)
-    map_tile_1_2 = MapTile.new('grass', false)
-    map_tile_2_2 = MapTile.new('grass', false)
-    map_grid = [
-        [map_tile_0_0, map_tile_1_0, map_tile_2_0],
-        [map_tile_0_1, map_tile_1_1, map_tile_2_1],
-        [map_tile_0_2, map_tile_1_2, map_tile_2_2]
-    ]
-    return map_grid
-end
+# map_tile_y_x where and y is the row and x is the column
+# so map_tile_2_1 would represent the second row, third column.
+# i have went against convention of x,y and did y,x instead so that we
+# can look at the 2D array and line up the indexes with it
+class Map
+    attr_reader(:map_grid, :winning_tile)
 
-def display_map(map_grid)
-    puts "-------------------"
-    # for each row, get the map symbol associated with the terrain of the MapTile object. surround by < > symbols to indicate current location of player (current location is stored in a flag in the map tile)
-    map_grid.each do |row|
-        puts "| #{row[0].player_is_here ? '<': ' '}#{row[0].get_map_symbol}#{row[0].player_is_here ? '>': ' '} | #{row[1].player_is_here ? '<': ' '}#{row[1].get_map_symbol}#{row[1].player_is_here ? '>': ' '} | #{row[2].player_is_here ? '<': ' '}#{row[2].get_map_symbol}#{row[2].player_is_here ? '>': ' '} |"
-        puts "-------------------"
+    def initialize
+        # FIXME hard coded instances for now, if have time will make these more random and use a loop
+        # FIXME use hashes to generate instances for code clarity and maintainability
+        @map_tile_0_0 = MapTile.new([0, 0], 'mountain', false)
+        @map_tile_1_0 = MapTile.new([1, 0], 'grass', false)
+        @map_tile_2_0 = MapTile.new([2, 0], 'mountain', true)
+        @map_tile_0_1 = MapTile.new([0, 1], 'grass', false)
+        @map_tile_1_1 = MapTile.new([1, 1], 'grass', false)
+        @map_tile_2_1 = MapTile.new([2, 1], 'mountain', false)
+        @map_tile_0_2 = MapTile.new([0, 2], 'mountain', false)
+        @map_tile_1_2 = MapTile.new([1, 2], 'grass', false)
+        @map_tile_2_2 = MapTile.new([2, 2], 'grass', false)
+        @map_grid = [
+            [@map_tile_0_0, @map_tile_0_1, @map_tile_0_2],
+            [@map_tile_1_0, @map_tile_1_1, @map_tile_1_2],
+            [@map_tile_2_0, @map_tile_2_1, @map_tile_2_2]
+        ]
+        # FIXME hard coded for now
+        @winning_tile = [2, 2]
     end
     
+    def display_map
+        puts "World Map:"
+        puts '-------------------'
+        # for each row, get the map symbol associated with the terrain of the MapTile object. surround by < > symbols to indicate current location of player (current location is stored in a flag in the map tile)
+        @map_grid.each do |row|
+            puts "| #{row[0].player_is_here ? '<': ' '}#{row[0].get_map_symbol}#{row[0].player_is_here ? '>': ' '} | #{row[1].player_is_here ? '<': ' '}#{row[1].get_map_symbol}#{row[1].player_is_here ? '>': ' '} | #{row[2].player_is_here ? '<': ' '}#{row[2].get_map_symbol}#{row[2].player_is_here ? '>': ' '} |"
+            puts '-------------------'
+        end
+        puts ''
+    end
+    
+    def move_location(direction)
+        x_coord = nil
+        y_coord = nil
+        @map_grid.each_with_index do |row, i|
+            row.each do |tile|
+                if tile.player_is_here
+                    y_coord = tile.coords[0]
+                    x_coord = tile.coords[1]
+                    tile.player_is_here = false
+                    # the second part of the && boolean condition ensures that if the player is on the edge,
+                    # that the position doesn't wrap around or create any other undesired
+                    # behaviour but simply tells the user they cannot move in that direction
+                    if (direction == 'north') && (y_coord != 0)
+                        y_coord += -1
+                        x_coord += 0
+                    elsif (direction == 'south') && (y_coord != @map_grid.length - 1)
+                        y_coord += 1
+                        x_coord += 0
+                    elsif (direction == 'east') && (x_coord != row.length - 1)
+                        y_coord += 0
+                        x_coord += 1
+                    elsif (direction == 'west') && (x_coord != 0)
+                        y_coord += 0
+                        x_coord += -1
+                    else
+                        p "You cannot move further #{direction} lest you fall off the edge of the earth!"
+                        puts ''
+                    end
+                end
+            end
+        end
+        @map_grid[y_coord][x_coord].player_is_here = true
+        return [y_coord, x_coord]
+    end
 end
 
 class MapTile
-    attr_reader(:terrain)
-    attr_accessor(:player_is_here)
+    attr_reader(:terrain, :coords)
+    attr_accessor(:player_is_here, :wild_objectmon)
 
-    def initialize(terrain, wild_objectmon)
+    def initialize(coords, terrain, wild_objectmon)
+        @coords = coords
         @terrain = terrain
-        @wild_objectmon = false
+        @wild_objectmon = wild_objectmon
         @player_is_here = false
     end
 
@@ -57,5 +169,14 @@ class MapTile
             # if there is an error just default to grass
             return '"'
         end
+    end
+end
+
+class Objectmon
+    def initialize(name, type, dmg, hp)
+        @name = name
+        @type = type
+        @dmg = (dmg[0]..dmg[1])
+        @hp = hp
     end
 end
