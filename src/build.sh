a504eb55724f60a40a6c8983ceed9da4a02c9aa6
LevPewPew <levtoth1988@gmail.com>
2019-09-05 12:24:25 +1000

    created a build file. also last commit before doing a rubocop auotfix

---
+++ src/build.sh
@@ -0,0 +1,13 @@
+# for linux users
+chmod +x ./build.sh
+# copy across files to distribution directory
+mkdir ../dist
+cp ./objectmon.rb ../dist
+cp ./game_system.rb ../dist
+cp ./default_high_scores.csv ../dist
+cp ../dist/default_high_scores.csv ../dist/high_scores.csv
+# install all gems
+gem install colorize
+gem install artii 
+gem install terminal-table 
+gem install tty-prompt 
